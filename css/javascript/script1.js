dda344cbcc5230e0bc28951d1b045ff40d9b92c7
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-05 19:24:32 +0530

    ..

---
+++ css/javascript/script1.js
@@ -0,0 +1,50 @@
+// let a=90;
+// let b ="jkdsjkfasnkfna"
+// let c=false
+
+// console.log(b);
+
+// console.log("hello world")
+
+// let firstName = prompt("Enter your first name")
+
+// let lastName =prompt("enter your last name")
+
+// let fullname=firstName + "  "+lastName
+
+// alert(fullname);
+
+
+let subjectOne = prompt("enter your first subject marks")
+let subjectTwo = prompt("enter your second subject marks")
+let subjectThree = prompt("enter your third subject marks")
+let subjectFour = prompt("enter your fourth subject marks")
+let subjectFive = prompt("enter your fifth subject marks")
+
+subjectOne =Number(subjectOne);
+subjectTwo=Number(subjectTwo);
+subjectThree =Number(subjectThree);
+subjectFour =Number(subjectFour);
+subjectFive =Number(subjectFive);
+
+
+let totalMarks = subjectOne+subjectTwo+subjectThree+subjectFour+subjectFive;
+
+alert(totalMarks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
