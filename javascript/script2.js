87090b941315407fc82fe60b2e19ed6f7069d416
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-07 19:30:00 +0530

    ..

---
+++ javascript/script2.js
@@ -0,0 +1,82 @@
+let rain =false;
+let umbrella =false;
+let raincoat =false;
+
+
+console.log(!rain || (rain&&umbrella) || (rain&&raincoat));
+
+
+
+
+//console.log("hello this is script 2")
+
+//logical operators
+
+//&& || !
+//unary opera => not
+// false =>true
+// true =>false
+// let bool1=false
+// let bool2=true
+
+// console.log(!((bool1 || bool2 && (bool1||bool1)) && bool2))
+// // console.log(bool1 || bool2)
+
+
+
+
+
+
+
+//comparision operators
+
+//< ,> ,>=,<= , ==, ===, !=,!==, 
+
+
+// let a=80;
+// let b=80;
+
+// let result = a<b
+
+// console.log(a<b)//false
+// console.log(a>b)//false
+// console.log(a<=b);//true
+// console.log(a>=b);//true
+//console.log(a!=b);//false
+
+// let c='9'
+// let d=9
+// console.log(c==d);//true
+// console.log(c===d);//falase
+// console.log(c==d,"this is the result od c==d");
+// let str="sdjkfsjadbfjsd" + "shjdfjsdjfsajdj"
+// console.log(str);
+// console.log("sdjkfsjadbfjsd" + "shjdfjsdjfsajdj")
+// console.log(9+"dkslfksafkds");
+
+// let num ="44";
+// num =Number(num);
+// num=num+8;
+// console.log(num)
+
+//console.log(result);
+
+
+
+
+//binary operator
+//declaration
+// let result10;
+// result10=90;
+// console.log("inside rsult10",result10 )
+
+
+// let result = 23+45
+// result = 23-45;
+// result = 45*54;
+
+// let result3=4/2;
+// let result2 = 5%3;
+// let result4 = 2**3; //2^3
+// console.log(result4)
+// console.log(3**4);
---
+++ javascript/script3.js
@@ -0,0 +1,103 @@
+ console.log("day 3 of javascript")
+
+ //ternary Operators
+
+ let a=190
+ let b=170
+ let c=89;
+ let d=76;
+ let result = a>b?(a+b):(a-b)
+ a>b?((c>d)?true:false):false
+ 
+ a==0?"Sunday":(a==1?"Monday":(a==2)?"Tues":"")
+
+console.log(result)
+
+// let a=90;
+// let b=90;
+// let c=90;
+
+// if(a>b && a>c){
+//     console.log(a);
+// }
+// else if(b>a && b>c){
+//     console.log(b);
+// }
+// else if(c>a && c>b){
+//     console.log(c);
+// }
+// else{
+//     console.log("all are equal")
+// }
+
+
+
+//  let rain =false;
+//  let umbrella =false;
+//  let raincoat =false;
+
+//  if(!rain){
+//     console.log("user go out without anything");
+//  }
+//  else if(rain && umbrella){
+//     console.log("user go out with umbrella")
+//  }
+//  else if(rain &&  raincoat){
+//     console.log("user can go out with raincoat");
+//  }
+//  else{
+//     console.log("user cannot go out");
+//  }
+
+
+//nested if
+
+// if(rain){
+    
+//     if(umbrella){
+//         console.log("user can go out with umbrella")
+//     }
+//     else{
+//         if(raincoat){
+//             console.log("user can go out with raincoat")
+//         }
+//         else{
+//             console.log("user cannot go out")
+//         }
+//     }
+
+// }
+// else{
+//     console.log("user can go out without anything")
+// }
+
+
+
+
+
+// if( (rain&&umbrella) || (rain&&raincoat) || !rain){
+//     console.log("you can go out")
+// }
+// else{
+//     console.log("you cannot go out")
+// }
+
+
+
+
+
+
+// let exp=34<23 || 34>45 && 233<23
+//  if(exp){
+//     console.log("hello world")
+//     console.log("hello world")
+
+//     console.log("hello world")
+//     console.log("hello world")
+//     console.log("hello world")
+//     console.log("hello world")
+//  }
+//  else{
+//     console.log("else part ")
+//  }
+
