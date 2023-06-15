a5650dbbdfdeffbda47cfa1f278f0d197a6e292b
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-15 19:28:06 +0530

    ..

---
+++ javascript/scriptDay2.js
@@ -0,0 +1,67 @@
+//&& || !
+
+// operand1 && operand2 =>Boolean(true / false)
+
+// operand1    operand2   &&      ||         ^(Xor)
+// true         true      true     true      false
+// false        true      false    true      true
+// true         false     false    true      true
+// false        false     false    false     false
+ 
+// !=> unary operator => 1operand
+
+// operand   !
+// false      true
+// true       false  
+
+
+// scenario => 18 +
+//             voterId
+//             Indian national
+
+
+// let num=90;
+// num=90+"";
+// num= num.toString();
+
+// console.log(num, typeof num)
+// let age = prompt("what is your age")
+// age = Number(age);
+// let nationality = prompt("are you an Indian (y/n)");
+// // 16-17=>school
+// // 18-24 => college or india (nationality)
+// // 25-50=> nation wide(nationality)
+// // 51 => cannot vote
+// if(age>=16 && age<=17){
+//     console.log("the person can vote in school")
+// }
+// else if ( age>=18 && age <=24 && nationality==="y"){
+//     console.log("the person vote national and in college");
+// }
+// else if(age >=25 && age<=50 && nationality==="y"){
+//     console.log("nation wide")
+// }
+// else{
+//     console.log("person cannot vote");
+// }
+
+
+
+// prompt => 4nuumber 
+// console => max of the four number
+
+
+
+
+
+
+
+
+
+//  let voterId = prompt("Do you have voter card or not(enter y or n)");
+// let citizen = prompt("Citizen of which country?")
+
+
+
+
+
