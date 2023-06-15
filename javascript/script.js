aabe5d7af7bfe80859379048d38be31a0edfc2d6
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-15 15:42:27 +0530

    ..

---
+++ javascript/script.js
@@ -0,0 +1,63 @@
+ < ,> ,<= ,>= , == ,!=,===,!==
+
+ let a=90;
+ let b='90';
+
+ console.log(a!==b)
+
+
+
+
+
+
+
+
+
+//console.log("hello world")
+
+// let a =23; //variable
+// let b=4;
+
+//arithmetic operator
+// let c = a+b;
+// let str = "90"+180
+// let obj ={a:"a"}
+// console.log(c);
+// console.log(10+10+"hello"+10+10)
+// console.log(str,obj)
+// alert("hello world")
+
+//  * ,/ ,% ,**
+// let sub= a-b; 
+// console.log(sub)
+// let exponent = 3**4;
+// console.log(exponent)
+// let mod = a%b;
+// console.log(mod);
+// let div= a/b;
+// console.log(div);
+// let multiplication = a*b;
+// console.log(multiplication);
+
+
+
+// console.log(a , " + ", b , " = " , c,{a:"b"})
+// let str="str"
+// console.log(a + " + " + b + " = " + c+{a:"b"});
+
+
+
+// console.log(a,typeof a);
+// a=true;
+// console.log(a,typeof a);
+// a={};
+// console.log(a,typeof a);
+// a=[];
+// console.log(a,typeof a);
+// a=function(){}
+// console.log(a,typeof a);
+// a="jasdhfjshdflk";
+// console.log(a,typeof a);
+
+
+
