718139330ce4f919130cd38c527860ad4f0848e8
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-19 19:23:53 +0530

    .'

---
+++ javascript/ternaryOperator.js
@@ -0,0 +1,110 @@
+
+// for(let i=0 ; true;){
+//     console.log("hello world");
+// }
+
+//table of 5
+
+for(let i=5;i<=50;i=i+5){
+    console.log(i)
+}
+
+for(let i=1;i<=50;i++){
+    if(i%5==0){
+        console.log(i);
+    }
+}
+
+for(let i=1;i<=10;i++){
+    console.log(5*i);
+}
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+* * * * *
+* * * *
+* * *
+* * 
+*
+
+
+
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+
+
+     *
+    **
+   ***
+  ****
+ *****
+
+ 
+
+ //operand1?operand2:operand3
+
+//  operand1=> expression boolean (true/false)
+//  operand2 =>any expression
+//  operand3=> any expression
+
+
+// let operator = prompt("add / sub");
+
+// let result =  operator==="add"?30+20:30-20;
+// console.log(result)
+
+//given these 3 number cant be equal
+// let number1 = Number(prompt("first number"))
+// let number2 = Number(prompt("Second number"))
+//  let number3= Number(prompt("third number"));
+// let x =prompt("enter number four");
+// x=Number(x);
+
+// let result = (number1>number2)?number1:(number2%2==0?number2:0)
+// console.log(result);
+
+// let result2 = number3>number1 && number3>number2 ? number3 
+//            :(number2>number1 && number2>number3? number2 :number1) ;
+// console.log(result2);
+
+// let w=90;
+// let x=45;
+// let y=34;
+// let z=56;
+
+// let result = w>x && w>y&& w>z ? w : (x>w && x>y &&x>z ? x : (y>w && y>x && y>z?y :z))
+
+
+// let arr=[23,23,4,54,5,52,64,5,34]
+// let max=arr[0];
+// for(let i=0;i<9;i++){
+//     if(max<arr[i]){
+//         max=arr[i]
+//     }
+// }
+// console.log(max)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
