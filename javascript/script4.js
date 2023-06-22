08fbec5fd7b25b6aacf70985a28450edb5cc3c49
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-22 19:26:35 +0530

    ..

---
+++ javascript/script4.js
@@ -0,0 +1,87 @@
+//arrow function
+
+let message=()=>{
+    console.log("hello iam also a function")
+}
+
+message();
+
+let message2=para1=>{
+    console.log(para1+"hello iam also a function")
+}
+
+message2(10.454);
+
+// let message3=(para1,para2)=>{
+//     console.log(para1+para2+"hello iam also a function")
+//     // return 20;
+// }
+
+// message3(10,20);
+//single line fucntion
+let message4=para1=>10
+
+let result =message4("iam a string");
+console.log(result)
+
+// let calculator = function(a,b,symbol){
+//         if(symbol==="+"){
+//             return a+b
+//         }
+//         else if(symbol ==="-"){
+//             return a-b
+//         }
+        
+// }
+
+// let result = calculator(12,13,"+");
+
+
+// console.log("hellow this is script 4")
+
+// function playGame(name){
+  
+    
+//     let score = Math.floor(Math.random()*10)
+//     console.log(name+" is playing and score is : "+score);
+//     return score;
+
+// }
+
+// function Average(sum , totalStudents)
+// {
+//     let avg= sum/totalStudents;
+//     console.log(avg);
+// }
+// let player1Score = playGame("rita");
+// let player2Score = playGame("priya")
+// let player3Score =playGame("meena");
+// let sum=player1Score+player2Score+player3Score;
+// Average(sum, 3);
+
+
+//why and how to use function
+
+// function addition(a,b){
+//     let sum=0;
+   
+//     sum=a+b;
+//     return sum;
+// }
+
+// let a =Number(prompt("enter first  number "))
+// let b=Number(prompt("enter second number"));
+
+// let a1= addition(10,20)
+// console.log(a1)
+// addition(30,20)
+// addition(20,60)
+// addition();
+// addition();
+// addition();
+
+
+
+
+
+ 
