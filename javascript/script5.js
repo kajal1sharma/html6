e1680bd28b2f597fce49fbffb4986750799b4c58
brainmentorspvtltd <kajal1231995@gmail.com>
2023-06-26 19:22:45 +0530

    ..

---
+++ javascript/script5.js
@@ -0,0 +1,80 @@
+console.log("this is our script5")
+
+
+
+
+// scoreCalculator(34,34,32);
+
+
+function printMessage(nn, fn){
+   console.log(fn)
+   let result=fn(34,45,56);
+   console.log(result)
+//    fn(23,45,32);
+//     console.log("percentage of "+name+);
+
+}
+let  scoreCalculator=function (a,b,c){
+    return (a+b+c)/3;
+}
+let n="tina"
+printMessage(n,scoreCalculator)
+
+// let arr = [1,2,3,4]
+
+// arr.push(5);
+// arr.push(6)
+// console.log(arr);
+// // let res= arr.pop();
+// // console.log(res);
+
+// arr.shift();
+// console.log(arr)
+// arr.unshift(9)
+// console.log(arr)
+
+
+
+
+//   {{{{{}}{{{}}}}}}
+//   {{}{}{}}
+//   {{{}
+
+// let arr=[1,2,3,4,5,6]
+// function for_each(callfn){
+
+//     for(let i=0;i<arr.length;i++){
+//         callfn(arr[i]);
+//     }
+// }
+
+// function callback(ele){
+//     console.log(ele+"=>")
+// }
+
+// for_each(callback);
+
+
+
+
+
+
+
+let arr=[1,2,3,4,5,6,7]
+
+
+
+
+// arr.forEach(function(ele){
+//     console.log(ele+".....")
+
+// })
+
+
+// let sum =0;
+
+// arr.forEach((ele)=>{
+//     sum=sum+ele
+// })
+
+// console.log(sum)
